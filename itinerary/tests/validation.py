85b4fcec177b1ec30c729f7d2c2a026df148043a
Clare Ulmer <clareulmer@uchicago.edu>
2018-11-20 20:45:04 -0600

    updated file structure

---
+++ itinerary/tests/validation.py
@@ -0,0 +1,150 @@
+
+
+''' TIME VALIDATION FUNCTIONS '''
+
+# checks that the times given for itin items don't overlap
+def validate_nooverlap(itin, user_times=0):
+    return False
+
+# checks that itin items are in chronological order
+def validate_chrono(itin):
+    return False
+
+# checks that times given for itin items are within event's open hours
+def validate_isopen(itin, day):
+    return False
+
+# checks that itin items fall within specified user times
+def validate_within_usertime(itin, user_times=0):
+    return False
+
+
+''' DISTANCE VALIDATION FUNCTIONS '''
+
+# check that each venue in the itinerary is within dist (in miles; optionally
+# specified by the user) from the user's starting location
+def validate_max_distance(itin, start_location, dist=10):
+    return False
+
+# check that each venue is within a reasonable distance of venues before
+# and after it
+# (dist is the same parameter as in the above function)
+# this function ensures that venues in the itinerary are clustered to some
+# extent, not just randomly spread out throughout the space defined by dist
+def validate_event_distance(itin, dist=10):
+    return False
+
+# check that the time between each pair of adjacent events in the itinerary
+# is sufficient for travel between their venues, given user's transportation mode
+def validate_travel_time(itin, transport="drive"):
+    return False
+
+
+''' EVENT VALIDATION FUNCTIONS '''
+
+def validate_no_duplicates(itinerary):
+    '''
+    check that there are no duplicates in the itinerary
+
+    inputs:
+        itinerary (list of dicts)
+
+    outputs:
+        0 for success
+        1 for failure
+    '''
+    ids = set()
+    for event in itinerary:
+        if event[0]['event_id'] in ids:
+            return 1
+        ids.add(event[0]['event_id'])
+
+    return 0
+
+
+def validate_restaurants(itinerary):
+    '''
+    check that there are is at least 1 restaurant if the itinerary goes on for
+    more than 4 hours (we can change this later)
+
+    inputs:
+        itinerary (list of dicts)
+
+    outputs:
+        0 for success
+        1 for failure
+    '''
+    start = itinerary[2]
+    end = itinerary[3]
+    if (end - start) >= (4 * 60):
+        for event in itinerary:
+            if 'food' in event[0]['tags']:
+                return 0
+        return 1
+
+    return 0
+
+
+def validate_venue_id_match(itinerary):
+    '''
+    check that the events and the venues have the same venue id
+
+    inputs:
+        itinerary (list of dicts)
+
+    outputs:
+        0 for success
+        1 for failure
+    '''
+    for event in itinerary:
+        if event[0]['venue_id'] != event[1]['venue_id']:
+            return 1
+
+    return 0
+
+
+def validate_event_date(itinerary, date):
+    '''
+    check that the events in the itinerary are valid for the date that the
+    user input/generated
+
+    inputs:
+        itinerary (list of dicts)
+        date (str) - 'mm-dd-yyyy' (could potentially change date format later)
+
+    outputs:
+        0 for success
+        1 for failure
+    '''
+    for event in itinerary:
+        # only need to check date for one time events
+        # permanent events checked by time
+        if 'date' in event[0]:
+            if date != event[0]['date']:
+                return 1
+
+    return 0
+
+
+def validate_event_price(itinerary, price):
+    '''
+    check that the event price is below the user specified price
+
+    inputs:
+        itinerary (list of dicts)
+        price (float)
+
+    outputs:
+        0 for success
+        1 for failure
+    '''
+    for event in itinerary:
+        p = event[0]['price']
+        if p == -10:
+            # there was no price, move on
+            pass
+        else:
+            if p > price:
+                return 1
+
+    return 0
---
+++ itinerary/tests/validation_tests.py
@@ -0,0 +1,86 @@
+import unittest
+from validation import *
+from itin_examples import *
+
+class TestTimeValidation(unittest.TestCase):
+    def test_validate_nooverlap(self):
+        self.assertTrue(validate_nooverlap(itin1))
+        self.assertFalse(validate_nooverlap(itin12))
+        self.assertFalse(validate_nooverlap(itin13))
+        self.assertFalse(validate_nooverlap(itin14))
+        self.assertFalse(validate_nooverlap(itin9))
+
+    def test_validate_chrono(self):
+        self.assertTrue(validate_chrono(itin1))
+        self.assertTrue(validate_chrono(itin10))
+        self.assertTrue(validate_chrono(itin12))
+        self.assertTrue(validate_chrono(itin13))
+        self.assertFalse(validate_chrono(itin9))
+        self.assertFalse(validate_chrono(itin14))
+
+    def test_validate_isopen(self):
+        self.assertTrue(validate_isopen(itin1,"mon"))
+        self.assertTrue(validate_isopen(itin1,"tues"))
+        self.assertTrue(validate_isopen(itin1,"wed"))
+        self.assertTrue(validate_isopen(itin1,"thurs"))
+        self.assertTrue(validate_isopen(itin1,"fri"))
+        self.assertTrue(validate_isopen(itin1,"sat"))
+        self.assertTrue(validate_isopen(itin1,"sun"))
+        self.assertFalse(validate_isopen(itin15,"mon"))
+        self.assertFalse(validate_isopen(itin15,"tues"))
+        self.assertFalse(validate_isopen(itin15,"wed"))
+        self.assertFalse(validate_isopen(itin15,"thurs"))
+        self.assertFalse(validate_isopen(itin15,"fri"))
+        self.assertFalse(validate_isopen(itin15,"sat"))
+        self.assertFalse(validate_isopen(itin15,"sun"))
+
+    def test_validate_within_usertime(self):
+        self.assertFalse(validate_within_usertime(itin1,[800,1000]))
+        self.assertFalse(validate_within_usertime(itin1,[630,850]))
+        self.assertTrue(validate_within_usertime(itin1,[600,950]))
+
+class TestDistanceValidation(unittest.TestCase):
+
+    def test_validate_max_distance(self):
+        self.assertTrue(validate_max_distance(itin1, [41.792210, -87.599940]))
+        self.assertFalse(validate_max_distance(itin8, [41.792210, -87.599940]))
+        self.assertTrue(validate_max_distance(itin8, [41.792210, -87.599940], 22))
+        self.assertFalse(validate_max_distance(itin1, [41.792210, -87.599940], 7))
+
+    def test_validate_event_distance(self):
+        self.assertTrue(validate_event_distance(itin1))
+        self.assertFalse(validate_event_distance(itin8))
+        self.assertTrue(validate_max_distance(itin8, 20))
+        self.assertFalse(validate_max_distance(itin1, 2))
+
+    def test_validate_travel_time(self):
+        self.assertFalse(validate_travel_time(itin1, "drive"))
+        self.assertTrue(validate_travel_time(itin2, "drive"))
+        self.assertFalse(validate_travel_time(itin3, "bike"))
+        self.assertTrue(validate_travel_time(itin4, "bike"))
+        self.assertFalse(validate_travel_time(itin4))
+        self.assertTrue(validate_travel_time(itin5))
+        self.assertFalse(validate_travel_time(itin6, "walk"))
+        self.assertTrue(validate_travel_time(itin7, "walk"))
+        self.assertFalse(validate_travel_time(itin11))
+
+class TestEventValidation(unittest.TestCase):
+    def test_validate_no_duplicates(self):
+        self.assertEqual(validate_no_duplicates(itin9),1)
+        self.assertFalse(validate_no_duplicates(itin10))
+
+    def test_validate_venue_id_match(self):
+        self.assertTrue(validate_venue_id_match(itin10))
+        self.assertFalse(validate_venue_id_match(itin8))
+
+    def test_validate_event_date(self):
+        self.assertTrue(validate_event_date(itin8, '02-16-2018'))
+        self.assertFalse(validate_event_date(itin8, '02-16-2019'))
+
+    def test_validate_event_price(self):
+        self.assertTrue(validate_event_price(itin8, 0))
+        self.assertFalse(validate_event_price(itin8, 100000))
+
+
+if __name__ == '__main__':
+    unittest.main()

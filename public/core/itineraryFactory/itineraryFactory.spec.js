cbd0cdea9ceccf424e5c84e38912b7f18c345a91
Leslie Jones-Dove <ljonesdove@cs.uchicago.edu>
2018-11-29 22:00:57 -0600

    test init

---
+++ public/core/itineraryFactory/itineraryFactory.spec.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('itineraryFactory', function() {
+
+  beforeEach(module('ItinGen'));
+
+  describe('itineraryFactory factory', function(){
+
+    it('should ....', inject(function($factory) {
+      //spec body
+      var ItinFactory = $factory('itineraryFactory');
+      expect(ItinFactory).toBeDefined();
+    }));
+
+  });
+});

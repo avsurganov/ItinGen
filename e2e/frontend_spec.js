ece4ec840774a7db4680a7ff4eaec66e386293f8
Vladimir Surganov <avsurganov@gmail.com>
2018-11-30 05:22:42 -0600

    possible work

--- e2e/frontend_spec.js
+++ e2e/frontend_spec.js
@@ -1,11 +1,10 @@
 module.exports = {
-  'Google\'s Search Functionality' : function (browser) {
+  'Title is Bing': function(browser) {
+    // Browser is the browser that is being controlled
     browser
-      .url('https://www.google.com/ncr')
-      .waitForElementVisible('body', 1000)
-      .setValue('input[type=text]', 'TestingBot\n')
-      .pause(1000)
-      .assert.title('TestingBot - Google Search')
-      .end();
-  }
-};
+      .url('https://www.bing.com') // Navigate to the url
+      .waitForElementVisible('body', 1000) // Wait until you can see the body element.
+      .verify.title('Bing') // Verify that the title is 'Bing'
+      .end() // This must be called to close the browser at the end
+    }
+}
